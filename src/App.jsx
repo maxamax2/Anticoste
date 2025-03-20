4f54af7baddc8eba8f38bbb073b56effab8a8f18
Ugo Nanini <2138936@csfoy.ca>
2025-03-20 15:30:21 -0400

    Ajout de router pour les différentes "pages"

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-const App = () => {};
+import { Route } from "react-router-dom";
+import Accueil from "./pages/Accueil";
+import APropos from "./pages/APropos";
+import RoseDesVents from "./pages/RoseDesVents";
+import Recit from "./pages/Recit";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Accueil />} />
+      <Route path="/a-propos" element={<APropos />} />
+      <Route path="/rose-des-vents" element={<RoseDesVents />} />
+      <Route path="/recit" element={<Recit />} />
+    </Routes>
+  );
+};
 
 export default App;
