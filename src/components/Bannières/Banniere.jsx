e3896814bbeebf599aab64c8e15156af6ac86281
Ugo Nanini <2138936@csfoy.ca>
2025-03-27 14:01:22 -0400

    Test d'affichage d'une banniere

--- src/components/Bannières/Banniere.jsx
+++ src/components/Bannières/Banniere.jsx
@@ -1,5 +1,12 @@
+import image1 from "../../assets/images/1.png";
+
 const Banniere = () => {
-  return <h1>Une bannière</h1>;
+  return (
+    <div>
+      <h1>Telle une grande baleine</h1>
+      <img src={image1} alt="Allo" />
+    </div>
+  );
 };
 
 export default Banniere;
