2c1911d3dd666136f207582c75ea3a57207f6055
Gugmaton <2138936@csfoy.ca>
2025-04-03 12:42:28 -0400

    Ajout de toutes les imagesp pour la page finale de la liste des récits

--- src/data/banniereData.js
+++ src/data/banniereData.js
@@ -11,22 +11,22 @@ const banniereData = [
     title: "Cimetière du golf",
     photoName: "../../assets/images/r3/01.jpg",
   },
-  // {
-  //   title: "Le grand châtelin",
-  //   photoName: "../../assets/images/r4/01.jpg",
-  // },
-  // {
-  //   title: "Nés sur le reef",
-  //   photoName: "../../assets/images/r5/01.jpg",
-  // },
-  // {
-  //   title: "Sont venus chercher",
-  //   photoName: "../../assets/images/r6/01.jpg",
-  // },
-  // {
-  //   title: "Des pas et des prises",
-  //   photoName: "../../assets/images/r7/01.jpg",
-  // },
+  {
+    title: "Le grand châtelin",
+    photoName: "../../assets/images/r4/01.jpg",
+  },
+  {
+    title: "Nés sur le reef",
+    photoName: "../../assets/images/r5/01.jpg",
+  },
+  {
+    title: "Sont venus chercher",
+    photoName: "../../assets/images/r6/01.jpg",
+  },
+  {
+    title: "Des pas et des prises",
+    photoName: "../../assets/images/r7/01.jpg",
+  },
 ];
 
 export default banniereData;
