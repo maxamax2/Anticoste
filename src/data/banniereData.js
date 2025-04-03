57960c36bd1fde5c1840e50e75e6e74ac55271fc
Gugmaton <2138936@csfoy.ca>
2025-04-03 12:36:10 -0400

    Insertion des images, mise à jour des données pour prototype

--- src/data/banniereData.js
+++ src/data/banniereData.js
@@ -1,32 +1,32 @@
 const banniereData = [
   {
     title: "Telle une grand baleine",
-    photoName: "/assets/images/1.png",
+    photoName: "/assets/images/r1/01.jpg",
   },
   {
     title: "Gardien de lumière",
-    photoName: "../../assets/images/1.png",
+    photoName: "../../assets/images/r2/01.jpg",
   },
   {
     title: "Cimetière du golf",
-    photoName: "../../assets/images/1.png",
+    photoName: "../../assets/images/r3/01.jpg",
   },
-  {
-    title: "Le grand châtelin",
-    photoName: "../../assets/images/1.png",
-  },
-  {
-    title: "Nés sur le reef",
-    photoName: "../../assets/images/1.png",
-  },
-  {
-    title: "Sont venus chercher",
-    photoName: "../../assets/images/1.png",
-  },
-  {
-    title: "Des pas et des prises",
-    photoName: "../../assets/images/1.png",
-  },
+  // {
+  //   title: "Le grand châtelin",
+  //   photoName: "../../assets/images/r4/01.jpg",
+  // },
+  // {
+  //   title: "Nés sur le reef",
+  //   photoName: "../../assets/images/r5/01.jpg",
+  // },
+  // {
+  //   title: "Sont venus chercher",
+  //   photoName: "../../assets/images/r6/01.jpg",
+  // },
+  // {
+  //   title: "Des pas et des prises",
+  //   photoName: "../../assets/images/r7/01.jpg",
+  // },
 ];
 
 export default banniereData;
