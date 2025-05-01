c23832416f207b971f44dec64a5247e315144378
Gugmaton <2138936@csfoy.ca>
2025-05-01 11:57:59 -0400

    Ajout de fonctionalité du précédent et suivant

--- src/components/subCarousel/SubCarousel.jsx
+++ src/components/subCarousel/SubCarousel.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import styles from "./subCarousel.module.css";
 
 const SubCarousel = ({ subCarousel, onClose }) => {
-  const firstItem = subCarousel[0]; // Display the first item for now
+  // const firstItem = subCarousel[0];
+  const [item, setItem] = useState(subCarousel[0]);
+
+  const handlePrev = () => {
+    const currentIndex = subCarousel.findIndex((i) => i === item);
+    const prevIndex =
+      (currentIndex - 1 + subCarousel.length) % subCarousel.length;
+    setItem(subCarousel[prevIndex]);
+  };
+
+  const handleNext = () => {
+    const currentIndex = subCarousel.findIndex((i) => i === item);
+    const nextIndex = (currentIndex + 1) % subCarousel.length;
+    setItem(subCarousel[nextIndex]);
+  };
 
   return (
     <div className={styles.subCarousel}>
@@ -10,13 +25,21 @@ const SubCarousel = ({ subCarousel, onClose }) => {
       </button>
       <img
         className={styles.image}
-        src={firstItem.photoName}
-        alt={firstItem.title || "SubCarousel Image"}
+        src={item.photoName}
+        alt={item.title || "SubCarousel Image"}
       />
       <div className={styles.info}>
-        <h2>{firstItem.title}</h2>
-        <p>{firstItem.texte}</p>
-        <small>{firstItem.reference}</small>
+        <h2>{item.title}</h2>
+        <small>{item.reference}</small>
+      </div>
+
+      <div className={styles.navigation}>
+        <button onClick={handlePrev} className={styles.prevButton}>
+          &lt;
+        </button>
+        <button onClick={handleNext} className={styles.nextButton}>
+          &gt;
+        </button>
       </div>
     </div>
   );
